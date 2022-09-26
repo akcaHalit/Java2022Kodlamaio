2c928556be040ecebff875b4e36f64efab073e48
Halit AKCA <halitakca070@gmail.com>
2022-09-26 15:09:57 +0300

    reCapDemo_Classes added

---
+++ reCapDemo_Classes/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       
+    	 DortIslem dortIslem = new DortIslem();
+         int result = dortIslem.topla(1, 8);
+         System.out.println(result);
+         int result2 = dortIslem.carp(7,9);
+         System.out.println(result2);
+    }  
+}
