bf2e67d5c427a5ce86edc033229d206f0f596625
Halit AKCA <halitakca070@gmail.com>
2022-09-26 15:05:46 +0300

    methodsOverloading added

---
+++ merthodsOverloading/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       
+    	 DortIslem dortIslem = new DortIslem();
+         int result = dortIslem.Topla(1, 8);
+         System.out.println(result);
+         int result2 = dortIslem.Topla(1,7,9);
+         System.out.println(result2);
+    }  
+}
